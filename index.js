41814073dba4872c5073ca49d491a58a1db0e26c
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-21 03:22:32 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+const Discord = require("discord.js");
+  const Midoking = new Discord.Client();
+    const Midokingtoken = (process.env.BOT_TOKEN);
+      Midoking.on('ready', () => {
+        Midoking.user.setGame(`Midoking`,'https://www.twitch.tv');
+          Midoking.log('Im Ready!');
+  
+        });
+
+  Midoking.on('message', message => {
+    if (message.content.split(' ')[0] == '%k')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   Midoking.on("message", message => {
+       var prefix = "%";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "b")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
+});
+
+client.login(process.env.BOT_TOKEN);
