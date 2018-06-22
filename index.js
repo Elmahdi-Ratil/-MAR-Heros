b38ae878ab5987d5b9bbe711d0d7475bef7bc17b
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-22 22:12:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,31 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Mido King |-bc `,"")
-client.user.setStatus("dnd")
+client.user.setGame(`Mido King |-bc `,"https://youtu.be/qdfP7jzpvZ8")
+
+  console.log('')
+  console.log('')
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log(`[Start] ${new Date()}`);
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log('')
+  console.log('+[------------------------------------]+');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('By The Atlantic Enjoy')
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('+[------------------------------------]+')
+  console.log('')
+  console.log('+[------------]+')
+  console.log(' Bot Is Online')
+  console.log('+[------------]+')
+  console.log('')
+  console.log('')
 });
 
 
