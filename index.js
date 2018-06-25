98b2ff2afe5d3faf695f65baa8d6cd5fccbf6aa9
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-25 21:41:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -69,4 +69,34 @@ client.on("guildMemberAdd", member => {
       });
       });
       });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Mido King |`,"https://youtu.be/qdfP7jzpvZ8")
+  console.log('')
+  console.log('')
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log(`[Start] ${new Date()}`);
+  console.log('+[-----------------------------------------------------------------]+')
+  console.log('')
+  console.log('+[------------------------------------]+');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('By The Atlantic Enjoy')
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('+[------------------------------------]+')
+  console.log('')
+  console.log('+[------------]+')
+  console.log(' Bot Is Online')
+  console.log('+[------------]+')
+  console.log('')
+  console.log('')
+});
+
+
 client.login(process.env.BOT_TOKEN);
